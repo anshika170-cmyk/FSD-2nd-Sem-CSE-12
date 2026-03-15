576a3df118c9664e19708cc021d5bb13ee9ad510
Anshika_Singh 2025b01011151 <singhanshi170@gmail.com>
2026-03-15 17:20:35 +0530

    Delete variables.js

--- variables.js
+++
@@ -1,8 +0,0 @@
-//containers store data
-//declared using var,let or const
-//var global scoped (oldest way to declare variables)
-//the final declared one will  be given as answer
-var a=10;
-console.log(a);
-//let and const use to provide values ;its a blocked parameter;block level
-//const can't be modified in a block
